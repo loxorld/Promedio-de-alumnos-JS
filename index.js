85b949ebe5b846343c868591870f112b1e258920
loxorld <brianlautaro@hotmail.com>
2022-08-09 23:23:08 -0300

    Primer Pre entrega

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-function ingresarAlumno (alum) {
+//Variables
+const alumnos = []
+/* const alumnoAux = new Alumno() */
+let condicion = "si"
+
+//Funciones
+function verificador(n1 = 0) {
+    while (isNaN(n1)) {
+        n1 = parseFloat(prompt("Ingrese porfavor una nota valida:"))
+    }
+    return n1
+}
+
+function ingresarAlumno () {
+    const alum = {
+        nombre:"",
+        apellido:"",
+        edad:0,
+        promedio:0
+    }
     let nota = 0
     let total = 0
     let promedio = 0
@@ -8,47 +27,30 @@ function ingresarAlumno (alum) {
     alum.apellido = prompt("Ingrese Apellido del alumno:")
     alum.edad = parseInt(prompt("Ingrese edad del alumno:"))
     nota = parseFloat(prompt("Ingrese su nota:"))
-    /* verificador (nota)   */
+    nota = verificador (nota)  
+    console.log(nota) 
     while (cond == "si"){
         total += nota 
         contador++
         cond = prompt("Ingrese Si si quiere seguir agregando notas").toLowerCase()
         if(cond == "si" ){
             nota = parseFloat(prompt("Ingrese su nota:"))
-            /* verificador (nota)  */
+            nota = verificador (nota)
         }
     }
     promedio = total / contador
     alum.promedio = promedio
+    alumnos.push(alum)
 }
 
-
-//Variables
-const alumnos = []
-/* const alumnoAux = new Alumno() */
-const alumnoAux = {
-    nombre:"",
-    apellido:"",
-    edad:0,
-    promedio:0
-}
-let condicion = "si"
 
 //Main
-ingresarAlumno(alumnoAux)
+
+ingresarAlumno()
 while (condicion == "si") {
-    alumnos.push(alumnoAux)
     condicion = prompt("Ingrese Si si quiere seguir agregando alumnos:").toLowerCase()
     if (condicion == "si"){
-        ingresarAlumno(alumnoAux)
+        ingresarAlumno()
     }
 }
-
 console.log(alumnos)
-
-
-
-
-
-
-
