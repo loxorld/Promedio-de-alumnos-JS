de8e8439a55c32710e7055b8d7b670a84421f90a
loxorld <brianlautaro@hotmail.com>
2022-08-23 22:47:25 -0300

    Pre Entrega 2

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 //Variables
-const alumnos = []
+let alumnos = []
 const idForm = document.getElementById("idForm")
 const agregarNota = document.getElementById("agregarNota")
 const botonAlumnos = document.getElementById("botonAlumnos")
 const divAlumnos = document.getElementById("divAlumnos")
 const notasAux = []
 let total = 0
+
+//clases
 class Alumno {
     constructor(nombre, apellido, edad, notas) {
         this.nombre = nombre
@@ -16,9 +18,14 @@ class Alumno {
     }
 }
 
+if(localStorage.getItem("alumnos")) { //Verifico si ya existe o no informacion
+    alumnos =  JSON.parse(localStorage.getItem("alumnos")) 
+} else {
+    localStorage.setItem("alumnos", JSON.stringify(alumnos)) 
+}   
 
 //Funciones
-agregarNota.addEventListener("click", (ev) => {
+agregarNota.addEventListener("click", (ev) => { //Listener para el boton agregar notas
     ev.preventDefault()
     notasAux.push(document.getElementById("nota").value)
 
@@ -26,34 +33,49 @@ agregarNota.addEventListener("click", (ev) => {
 
 idForm.addEventListener("submit", (e) => {
     e.preventDefault()
-    const alumno = new Alumno(document.getElementById("nombre").value, document.getElementById("apellido").value, document.getElementById("edad").value, notasAux)
+    const datForm = new FormData(e.target)
+    const alumno = new Alumno(datForm.get("nombre"), datForm.get("apellido"), datForm.get("edad"), notasAux)
     alumno.notas.forEach((nota) => { 
         total += parseInt(nota)
     })
     alumno.promedio = total / alumno.notas.length
     console.log(alumno.promedio)
-    alumnos.push(alumno)
+    if ((alumno.nombre != "") && (alumno.apelldio != "") && (alumno.edad != 0) && (alumno.promedio <= 10)) { // if que chequea que los datos ingresados sean coherentes
+        alumnos.push(alumno)
+        localStorage.setItem('alumnos', JSON.stringify(alumnos))
+    }
+    
     idForm.reset()
     total = 0 // vuelvo a poner el total en 0 para el proximo alumno
-    console.log(alumnos)
     notasAux.splice(0, notasAux.length)  // vacio el array para el proximo alumno
 })
 
-botonAlumnos.addEventListener("click", () => {
+botonAlumnos.addEventListener("click", () => {  //Listener para mostrar los alumnos cargados en la pagina
+    const almStorage = JSON.parse(localStorage.getItem('alumnos'))
     divAlumnos.innerHTML = ""
-    alumnos.forEach((alumno, indice) => {
+    almStorage.forEach((alumno, indice) => {
         divAlumnos.innerHTML += `
         <div class="card" id="alumno${indice}" style="width: 18rem;margin:3px;">
-        <div class="card-body">
-            <h5 class="card-title">Alumno ${alumno.nombre} ${alumno.apelldio}</h5>
-            <p class="card-text">Edad: ${alumno.edad}</p>
-            <p class="card-text">Notas: ${alumno.notas}</p>
-            <p class="card-text">Promedio:${alumno.promedio}</p>
-        </div>
+            <div class="card-body">
+                <h5 class="card-title">Alumno ${alumno.nombre} ${alumno.apelldio}</h5>
+                <p class="card-text">Edad: ${alumno.edad}</p>
+                <p class="card-text">Promedio:${alumno.promedio}</p>
+                <button class="btn btn-danger">Eliminar</button>
+            </div>
         </div>
         `
     })
+    almStorage.forEach((alumno, indice) => { // forma de llegar al boton eliminar en cada alumno
+        const tarjetaAlumno = document.getElementById(`alumno${indice}`)
+    
+        tarjetaAlumno.children[0].children[3].addEventListener('click', () => {
+            tarjetaAlumno.remove() //DOM
+            alumnos.splice(indice, 1) //Array
+            localStorage.setItem('alumnos', JSON.stringify(alumnos)) //Local storage
+        })
+    })  
 })
+
 
 
 
